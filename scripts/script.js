b36ffcbe1f4293b6573e4e75067a26a63f7e120e
AnaiLyu <lyuanai88@gmail.com>
2025-03-11 00:26:45 +0200

    feat: add dark mode

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,21 @@
+/** Dark Mode */
+const body = document.querySelector('.body');
+const darkToggle = document.querySelector('.dark-toggle');
+
+const darkMode = () => {
+  body.classList.toggle('dark-mode');
+  if (body.classList.contains('dark-mode')) {
+    darkToggle.textContent = '1';
+    body.classList.remove('light-mode');
+  } else {
+    darkToggle.textContent = '0';
+    body.classList.add('light-mode');
+  }
+};
+
+darkToggle.addEventListener('click', darkMode);
+
+/** Quotes */
 const quoteElement = document.querySelector('.quote');
 const generateBtn = document.querySelector('.generate-quote_btn');
 
