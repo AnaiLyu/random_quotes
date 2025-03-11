571536fde15ff84eb7c57b9e7a732912c81f6a5d
AnaiLyu <lyuanai88@gmail.com>
2025-03-12 00:18:02 +0200

    feat: add check if the quote is a favorite

--- scripts/script.js
+++ scripts/script.js
@@ -21,6 +21,7 @@ darkToggle.addEventListener('click', darkMode);
 const quoteElement = document.querySelector('.quote');
 const authorElement = document.querySelector('.author');
 const generateBtn = document.querySelector('.generate-quote_btn');
+const favoriteBtn = document.querySelector('.favorite_btn');
 
 let currentQuoteIndex = -1;
 
@@ -39,23 +40,46 @@ const getRandomQuote = () => {
 generateBtn.addEventListener('click', getRandomQuote);
 
 /** Make Favorite */
-const toggleFavorite = () =>
-  (quotes[currentQuoteIndex].isFavorite =
-    !quotes[currentQuoteIndex].isFavorite);
-
-/** Favorite */
-const favorite = document.querySelector('.favorite_btn');
-
-const makeFavorite = () => {
+const toggleFavorite = () => {
+  const currentQuote = quotes[currentQuoteIndex];
+  currentQuote.isFavorite = !currentQuote.isFavorite;
+  console.log(quotes);
   if (currentQuoteIndex !== -1) {
-    favorite.classList.toggle('favorite_btn_active');
+    favoriteBtn.classList.add('favorite_btn_active');
   }
-  if (favorite.classList.contains('favorite_btn_active')) {
-    favorite.textContent = 'Remove';
+  if (currentQuote.isFavorite) {
+    favoriteBtn.textContent = 'Remove';
+    favoriteBtn.classList.add('favorite_btn_active');
   } else {
-    favorite.textContent = 'Favorite';
+    favoriteBtn.textContent = 'Favorite';
+    favoriteBtn.classList.remove('favorite_btn_active');
   }
 };
 
-favorite.addEventListener('click', makeFavorite);
-favorite.addEventListener('click', toggleFavorite);
+/** Favorite */
+// const makeFavorite = () => {
+// if (currentQuoteIndex !== -1) {
+//   favoriteBtn.classList.add('favorite_btn_active');
+// }
+// if (currentQuote.isFavorite !== true) {
+//   favoriteBtn.classList.remove('favorite_btn_active');
+// }
+// if (favoriteBtn.classList.contains('favorite_btn_active')) {
+//   favoriteBtn.textContent = 'Remove';
+// } else {
+//   favoriteBtn.textContent = 'Favorite';
+// }
+// };
+const isFavoriteThisQuote = () => {
+  if (quotes[currentQuoteIndex].isFavorite) {
+    favoriteBtn.textContent = 'Remove';
+    favoriteBtn.classList.add('favorite_btn_active');
+  } else {
+    favoriteBtn.textContent = 'Favorite';
+    favoriteBtn.classList.remove('favorite_btn_active');
+  }
+};
+
+// favoriteBtn.addEventListener('click', makeFavorite);
+favoriteBtn.addEventListener('click', toggleFavorite);
+generateBtn.addEventListener('click', isFavoriteThisQuote);
